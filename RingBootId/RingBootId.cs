41d45afe1e8095a1622ac65ca10675cec1c5f77b
Raki Saionji <rakisaionji@gmail.com>
2019-09-30 01:42:58 -0600

    First commit and release

---
+++ RingBootId/RingBootId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RingBootId
+{
+    struct RingBootId
+    {
+        public string AppId;
+        public string AppName;
+        public RingAppInfo[] AppInfo;
+    }
+
+    struct RingAppInfo
+    {
+        public string AppId;
+        public Version Version;
+        public DateTime Date;
+
+        public override string ToString()
+        {
+            return String.Format("{0} Ver.{1:0}.{2:00} {3:yyyy/MM/dd HH:mm:ss}", AppId, Version.Major, Version.Minor, Date);
+        }
+    }
+}
